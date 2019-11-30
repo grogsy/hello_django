ffd35639150922b45b826de0741f187829d47e28
Brian <a.gray.cloud@gmail.com>
2019-11-30 05:41:14 -0400

    create test cases for about & home

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+class TestPages(SimpleTestCase):
+    def test_home_page(self):
+        res = self.client.get('/')
+        # Home page should return 200
+        self.assertEqual(res.status_code, 200)
+
+    def test_about_page(self):
+        res = self.client.get('/about/')
+        # About page should return 200
+        self.assertEqual(res.status_code, 200)
+        
